c6ad80961412d404d7c13c2a0cac2483e66f5d92
b-lemy <lemabrian1234@gmail.com>
2023-07-07 05:39:40 -0700

    readme

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-''' lockbox module '''
+""" Lockboxes """
 
 
 def canUnlockAll(boxes):
-    '''
-        CanUnockAll
-        ([boxes]): a list of list
-    '''
-
-    # initialize a list of unlocked boxes
-    unlocked = [False] * len(boxes)
-    # set the first box oprn
-    unlocked[0] = True
-    # iterate over the boxes
-    for index, box in enumerate(boxes):
-        # check if the box is unlocked
-        if unlocked[index]:
-            # get the keys in the box
-            for index, key in enumerate(box):
-                # set the box with a found key to open
-                if key < len(unlocked):
-                    unlocked[key] = True
-                    # get the keys at the box that has been opened
-                    # set the boxes with the keys to be open
-                    for i in boxes[key]:
-                        unlocked[i] = True
-    return all(unlocked)
+    """ LockBoxes Function """
+    T = []
+    for i in range(1, len(boxes)):
+        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
+        if i in T:
+            T = []
+        else:
+            return False
+    return True
